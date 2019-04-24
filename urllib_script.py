87f96e03cf8fc4bdd8d8f8757f14d02371cb7a77
Princu Jain <pjain@Princus-MacBook-Pro.local>
2019-04-24 20:31:21 +0530

    Updated the imports

--- urllib_script.py
+++ urllib_script.py
@@ -1,36 +1,36 @@
 #Running script using urllib2
 import sys
 import time
-import urllib2
-from BeautifulSoup import BeautifulSoup
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 import re
 import smtplib
 from email.mime.text import MIMEText
 
-url = #<Movie's link where all theatres are present>
+url = "https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100668-MT/20190501" #<Movie's link where all theatres are present>
 username = #<username>
 password = #<password>
 sender = #<sender's email>
-receivers = [<List of people whom you want to send>]
+receivers = [<List of recievers>]
 message = MIMEText("""Head over here to check if the script worked correctly. :P
-<link of movie>""")
-message['Subject'] = "Yo!!. We are on :D"
+https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100668-MT/20190501""")
+message['Subject'] = "Yo!!. We are on, book tickets now, it's available now :D"
 message['From'] = "GoGoMaster<GoGoMaster@GoGo.com>"
 message['To'] = ", ".join(receivers)
 #Yes, google has a open SMTP server :D
 smtpObj = smtplib.SMTP('smtp.gmail.com:587')
-
 while True:
-	page = urllib2.urlopen(url).read()
-	soup = BeautifulSoup(page)
-	for element in soup.findAll(attrs = {'class': re.compile(r"listing")}):
-		ele = element.findAll(text = re.compile("Inorbit"))
-		if ele:
-			smtpObj.ehlo()
-			smtpObj.starttls()
-			smtpObj.login(username, password)
-			smtpObj.sendmail(sender, receivers, message.as_string())
-			smtpObj.quit()
-			print "Successfully sent email"
-			sys.exit()
-	time.sleep(5)
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req).read()
+        soup = BeautifulSoup(page,features="html.parser")
+        for element in soup.findAll(attrs = {'class': re.compile(r"listing")}):
+            ele = element.findAll(text = re.compile("PVR: VR Bengaluru, Whitefield Road"))
+            if ele:
+                smtpObj.ehlo()
+                smtpObj.starttls()
+                smtpObj.login(username, password)
+                smtpObj.sendmail(sender, receivers, message.as_string())
+                smtpObj.quit()
+                print ("Successfully sent email")
+                sys.exit()
+                time.sleep(5)
